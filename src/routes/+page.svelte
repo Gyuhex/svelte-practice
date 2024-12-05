82e546123b5d41c2f8818c7b6bd69d1cc0e80a35
Gyuhex <gyuhexmeow7@gmail.com>
2024-12-05 22:25:18 +0800

    First Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Header from "./Header.svelte";
+
+    let loggedUser = $state('Ed');
+    let users = $state([ {id: 1, userName: 'eduardo'} ])
+    let newUser = $state('');
+    
+    function addUser () {
+       // Make sure newUser is not empty before adding
+       if (newUser) {
+            const newId = users.length + 1; // Generate new ID based on the current list size
+            users = [...users, { id: newId, userName: newUser }]; // Add the new user to the list
+            newUser = ''; // Clear the input field after adding the user
+        }
+
+    }
+</script>
+
+<Header {loggedUser} />
+
+<ul>
+    {#each users as user (user.id)}
+        <li>{user.userName}</li>
+    {/each}
+</ul>
+
+<input type="text" bind:value={newUser}>
+<button type="submit" onclick={addUser}>Add New User</button>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+    let { loggedUser } = $props();
+</script>
+
+<h1>Welcome {loggedUser}</h1>
+
+<style>
+    h1 {
+        background-color: rgb(224, 139, 139);
+    }
+</style>
